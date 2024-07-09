ad5b7951b9ac638b94dc04e9cc41f5c8ce2cfc0a
MyNameIsTrez <welfje@gmail.com>
2024-07-10 00:02:20 +0200

    Tile enum

--- main.c
+++ main.c
@@ -7,17 +7,57 @@
 #define MAX_HEIGHT 420
 #define MAX_WIDTH 420
 
-char map[MAX_HEIGHT][MAX_WIDTH];
+enum tile {
+	FLOOR,
+	WALL,
+	BOX,
+	STORAGE,
+	PLAYER,
+};
+
+enum tile map[MAX_HEIGHT][MAX_WIDTH];
 
 size_t width = 0;
 size_t height = 0;
+
+static char tile_to_char(enum tile t) {
+	switch (t) {
+		case FLOOR:
+			return ' ';
+		case WALL:
+			return '#';
+		case BOX:
+			return '$';
+		case STORAGE:
+			return '.';
+		case PLAYER:
+			return '@';
+	}
+	abort();
+}
+
+static enum tile char_to_tile(char c) {
+	switch (c) {
+		case ' ':
+			return FLOOR;
+		case '#':
+			return WALL;
+		case '$':
+			return BOX;
+		case '.':
+			return STORAGE;
+		case '@':
+			return PLAYER;
+	}
+	abort();
+}
 
 static void print_map(void) {
 	printf("width: %zu\n", width);
 	printf("height: %zu\n", height);
 	for (size_t y = 0; y < height; y++) {
 		for (size_t x = 0; x < width; x++) {
-			printf("%c", map[y][x]);
+			printf("%c", tile_to_char(map[y][x]));
 		}
 		printf("\n");
 	}
@@ -30,7 +70,7 @@ int main(void) {
 	while (getline(&line, &n, stdin) > 0) {
 		size_t len = 0;
 		while (line[len] != '\n') {
-			map[height][len] = line[len];
+			map[height][len] = char_to_tile(line[len]);
 			len++;
 		}
 		width = len > width ? len : width;
