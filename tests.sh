668cb5c6a32afc226a13f5fbd9ce32089ea89166
MyNameIsTrez <welfje@gmail.com>
2024-07-10 03:31:16 +0200

    Exit if compilation fails

--- tests.sh
+++ tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# gcc main.c -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -Ofast -march=native
 gcc main.c -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined
+
+if [[ $? -ne 0 ]]
+then
+	echo "Compilation failed"
+	exit 1
+fi
 
 < maps/level_963.txt ./a.out
 # < maps/up_twice.txt ./a.out
