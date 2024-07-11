dee35bc442dc0652f4495f9a272083b36d957eb4
MyNameIsTrez <welfje@gmail.com>
2024-07-11 20:15:36 +0200

    Add test

--- tests.sh
+++ tests.sh
@@ -6,8 +6,8 @@
 # gcc bfs.c -lm -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -Ofast -march=native
 # gcc bfs.c -lm -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined
 
-gcc area.c -lm -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -Ofast -march=native
-# gcc area.c -lm -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined
+# gcc area.c -lm -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -Ofast -march=native
+gcc area.c -lm -Wall -Wextra -Werror -Wpedantic -Wshadow -Wfatal-errors -g -fsanitize=address,undefined
 
 if [[ $? -ne 0 ]]
 then
@@ -18,12 +18,13 @@ fi
 # < maps/area_grows.txt ./a.out
 # < maps/area_requires_hashing.txt ./a.out
 # < maps/area_shrinks.txt ./a.out
+# < maps/area_undo_floodfill_is_hard.txt ./a.out
 # < maps/box_on_storage.txt ./a.out
 # < maps/down_twice.txt ./a.out
 # < maps/left_twice.txt ./a.out
 # < maps/level_963.txt ./a.out
 # < maps/level_40862.txt ./a.out
 # < maps/level_47601.txt ./a.out
-< maps/right_twice.txt ./a.out
+# < maps/right_twice.txt ./a.out
 # < maps/sokoban_on_storage.txt ./a.out
-# < maps/up_twice.txt ./a.out
+< maps/up_twice.txt ./a.out
